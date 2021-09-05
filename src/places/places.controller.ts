7d7b19de4a70a63c3ceb5d1d66f63a4607c5c6a1
ammb95 <arthurbdev@gmail.com>
2021-09-05 11:51:45 +0200

    adds findOne, update, remove and like endpoints

--- src/places/places.controller.ts
+++ src/places/places.controller.ts
@@ -4,17 +4,22 @@ import {
   Post,
   Body,
   UseInterceptors,
+  Param,
+  Patch,
+  Delete,
+  Put,
   // Patch, Param, Delete
 } from '@nestjs/common';
 import { PlacesService } from './places.service';
 import { CreatePlaceDto } from './dto/create-place.dto';
 import { PlacesInterceptor } from './places.interceptor';
+import { UpdatePlaceDto } from './dto/update-place.dto';
 // import { UpdatePlaceDto } from './dto/update-place.dto';
-@UseInterceptors(PlacesInterceptor)
 @Controller('places')
 export class PlacesController {
   constructor(private readonly placesService: PlacesService) {}
 
+  @UseInterceptors(PlacesInterceptor)
   @Post()
   create(@Body() createPlaceDto: CreatePlaceDto) {
     return this.placesService.create(createPlaceDto);
@@ -25,18 +30,23 @@ export class PlacesController {
     return this.placesService.findAll();
   }
 
-  // @Get(':id')
-  // findOne(@Param('id') id: string) {
-  //   return this.placesService.findOne(+id);
-  // }
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.placesService.findOne(+id);
+  }
 
-  // @Patch(':id')
-  // update(@Param('id') id: string, @Body() updatePlaceDto: UpdatePlaceDto) {
-  //   return this.placesService.update(+id, updatePlaceDto);
-  // }
+  @Put(':id')
+  update(@Param('id') id: string, @Body() updatePlaceDto: UpdatePlaceDto) {
+    return this.placesService.update(+id, updatePlaceDto);
+  }
 
-  // @Delete(':id')
-  // remove(@Param('id') id: string) {
-  //   return this.placesService.remove(+id);
-  // }
+  @Patch(':id/like')
+  like(@Param('id') id: string) {
+    return this.placesService.like(+id);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.placesService.remove(+id);
+  }
 }
--- src/places/places.service.ts
+++ src/places/places.service.ts
@@ -1,6 +1,6 @@
-import { Body, Inject, Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { CreatePlaceDto } from './dto/create-place.dto';
-// import { UpdatePlaceDto } from './dto/update-place.dto';
+import { UpdatePlaceDto } from './dto/update-place.dto';
 import { Place } from './place.entity';
 
 @Injectable()
@@ -10,7 +10,7 @@ export class PlacesService {
     private placesRepository: typeof Place,
   ) {}
 
-  async create(@Body() createPlaceDto: CreatePlaceDto) {
+  async create(createPlaceDto: CreatePlaceDto) {
     return this.placesRepository.create<Place>(createPlaceDto);
   }
 
@@ -18,15 +18,27 @@ export class PlacesService {
     return this.placesRepository.findAll<Place>();
   }
 
-  // findOne(id: number) {
-  //   return `This action returns a #${id} place`;
-  // }
+  async findOne(id: number): Promise<Place> {
+    return this.placesRepository.findByPk(id);
+  }
 
-  // update(id: number, updatePlaceDto: UpdatePlaceDto) {
-  //   return `This action updates a #${id} place`;
-  // }
+  async like(id: number) {
+    const place = await this.placesRepository.findByPk(+id);
+    const likes = place.likes + 1;
+    return this.placesRepository.update(
+      { likes },
+      { returning: true, where: { id } },
+    );
+  }
 
-  // remove(id: number) {
-  //   return `This action removes a #${id} place`;
-  // }
+  async update(id: number, updatePlaceDto: UpdatePlaceDto) {
+    return this.placesRepository.update(updatePlaceDto, {
+      returning: true,
+      where: { id },
+    });
+  }
+
+  async remove(id: number) {
+    return this.placesRepository.destroy({ where: { id } });
+  }
 }
